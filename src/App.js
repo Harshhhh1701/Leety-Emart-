ce7a4171ad0abc0a8a5f92651aa9a9a76caf24d1
Harsh <valambeharsh1701@gmail.com>
2022-07-27 00:48:12 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Hero from './Components/hero';
 import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
       <Navbar />
+      <Hero/>
     </div>
   );
 }
