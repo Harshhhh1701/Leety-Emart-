f8188c0849c45a848d8f51e65c874ef969e3051d
Harsh <valambeharsh1701@gmail.com>
2022-07-26 13:56:27 +0530

    navbar

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex border-b-2 border-gray-300 '>
+        <div className='flex w-1/4 border-r-2 border-gray-300'>
+                <div className="font-bold">LEETY</div>
+        </div>
+        <div className="flex">
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
