5c7f41e61f23b03f4184e0c701e3218b7f57f99f
Harsh <valambeharsh1701@gmail.com>
2022-07-28 00:10:47 +0530

    ok

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import bgImg from '../images/main.png'
 const Hero = () => {
   return (
-    <div className='flex h-[1280px] w-full'>
-      <div className="text-5xl text-white ">LEETY</div>
+    <div className='flex h-screen w-full items-center'>
+      <div className="flex flex-col w-3/4 items-center">
+        <div className="text-8xl tracking-widest font-bold font-serif text-white ">LEETY</div>
+        <div className="text-3xl tracking-wider font-sans font-light text-white"> Energy Drink</div>
+      </div>
+      <div className='w=1/4'>
+        <img src={bgImg} alt=""  />
+      </div>
     </div>
   )
 }
