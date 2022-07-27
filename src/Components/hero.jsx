b1ecda74685ca205ede9a5e24f578fd3bcf49baa
Harsh <valambeharsh1701@gmail.com>
2022-07-27 22:10:29 +0530

    ok

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>hero</div>
+    <div className='flex h-[1280px] w-full'>
+      <div className="text-5xl text-white ">LEETY</div>
+    </div>
   )
 }
 
