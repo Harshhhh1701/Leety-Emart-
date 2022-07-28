3edefe963ab735cd8d5a2e5ae833e359351811a6
Harsh <valambeharsh1701@gmail.com>
2022-07-28 11:51:12 +0530

    ok

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <div className='flex h-screen w-full items-center'>
       <div className="flex flex-col w-3/4 items-center">
-        <div className="text-8xl tracking-widest font-bold font-serif text-white ">LEETY</div>
-        <div className="text-3xl tracking-wider font-sans font-light text-white"> Energy Drink</div>
+        <div className="text-8xl tracking-widest font-bold font-serif text-white  animate-bounce ">LEETY</div>
+        <div className="text-3xl tracking-wider font-sans font-light text-white animate-pulse"> Energy Drink</div>
       </div>
       <div className='w=1/4'>
         <img src={bgImg} alt=""  />
