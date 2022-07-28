682aab1a8bc7853d81097bfc9c08896d845ebb22
Harsh <valambeharsh1701@gmail.com>
2022-07-28 11:29:45 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Features from './Components/features';
 import Hero from './Components/hero';
 import Navbar from './Components/navbar';
 
 function App() {
   return (
-    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
+    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-screen">
       <Navbar />
       <Hero/>
+      <Features/>
     </div>
   );
 }
